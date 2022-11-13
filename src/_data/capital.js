e3de2c6122d92198a2ab22b6d501dfeef9e6d39d
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2022-11-13 17:50:46 +0100

    Förökte fixa coat OfArms bild. Gick inte bra

--- src/_data/capital.js
+++ src/_data/capital.js
@@ -2,7 +2,7 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 require('dotenv').config();
 
 module.exports = async function() {
-    let url = `https://restcountries.com/v3.1/capital/stockholm`;
+    let url = `https://restcountries.com/v3.1/all`;
 
     /* This returns a promise */
     return EleventyFetch(url, {
